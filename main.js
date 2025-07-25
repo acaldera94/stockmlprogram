c111a8bd0d795a48cc7718f157fb9a816c18f50e
acaldera94 <artie.caldera@gmail.com>
2025-07-25 12:53:28 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,17 +2,15 @@ async function loadModel() {
   const model = await tf.loadLayersModel('./model/model.json');
   return model;
 }
-
 function getDummyInput(ticker) {
-  const baseInputs = {
+  const inputs = {
     AAPL: [0.5,0.2,0.8,0.6,0.7,0.3,0.1,0.9,0.4,0.5,0.6,0.7,0.8,0.5,0.3],
     TSLA: [0.4,0.1,0.6,0.7,0.5,0.2,0.3,0.8,0.4,0.6,0.7,0.6,0.7,0.5,0.4],
     MSFT: [0.6,0.3,0.7,0.5,0.6,0.4,0.2,0.7,0.3,0.5,0.5,0.6,0.6,0.4,0.3]
   };
-  const inputs = baseInputs[ticker.toUpperCase()] || baseInputs["AAPL"];
-  return tf.tensor2d([inputs]);
+  const input = inputs[ticker.toUpperCase()] || inputs["AAPL"];
+  return tf.tensor2d([input]);
 }
-
 async function predict() {
   const ticker = document.getElementById('ticker').value.trim().toUpperCase();
   const model = await loadModel();
@@ -24,7 +22,6 @@ async function predict() {
     updateChart(value, ticker);
   });
 }
-
 function updateChart(predictedValue, ticker) {
   const ctx = document.getElementById('stockChart').getContext('2d');
   const data = {
